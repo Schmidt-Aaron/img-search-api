b4a4af9780036adf17501137deba58037f68449d
Schmidt-Aaron <aschmidt808@gmail.com>
2017-07-10 17:12:31 -0700

    search api working

--- routes/search.js
+++ routes/search.js
@@ -7,12 +7,44 @@ var db = require("./db");
 router.get('/:query', function(req, res, next) {
   //query string
   let query = req.params.query;
-  
+  console.log(`Searching for ${query} pictures`);
+  //let timeStamp = new Date;
+
   //optional parameters
-  let page = req.query.offset;
+  let resultsPerPage = 10;
+  let page = 1 || req.query.offset * resultsPerPage;
   
-  console.log(query);
-  res.send(query);
+  var googleSearch = new Search({
+    key: process.env.API_KEY,
+    cx: process.env.CX
+  });
+
+  googleSearch.build({
+    q: query,
+    start: page,
+    num: resultsPerPage,
+    searchType: "image"
+  }, function(error, response) {
+    res.send(parseResults(response));
+  });
+
+  //result builder function
+  function parseResults(data) {
+    let prettyResults = [];
+    
+    //adjust this to change output
+    data.items.forEach(x => {
+        var item = {
+            title: x.title,
+            imageURL: x.link,
+            pageURL: x.image.contextLink
+        }
+        console.log(item);
+        prettyResults.push(item);
+    })
+    return prettyResults;
+  }
+  
 });
 
 module.exports = router;
